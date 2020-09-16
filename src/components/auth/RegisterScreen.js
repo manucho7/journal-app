447a9f35e43f2b1cefafeb2482363f04a004dd88
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-16 10:42:39 -0300

    Subiendo estilos para ser reutilizados en LoginScreen y RegisterScreen

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const RegisterScreen = () => {
     return (
         <div>
-            <h1>LoginScreen</h1>
+            <h1>RegisterScreen</h1>
         </div>
         
     )
