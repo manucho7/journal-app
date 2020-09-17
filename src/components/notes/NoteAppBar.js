0a555a7ce3ecd38f9c6ebd96fee32241606373a2
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-17 10:25:45 -0300

    NoteAppBar component and style

---
+++ src/components/notes/NoteAppBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const NoteAppBar = () => {
+    return (
+        <div className="notes__appbar">
+            <span>18 de septiembre</span>
+
+            <div>
+                <button className="btn">
+                    Picture
+                </button>
+
+                <button className="btn">
+                    Save
+                </button>
+            </div>
+
+        </div>
+    )
+}
