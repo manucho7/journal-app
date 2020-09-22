3d12e98dbee910a4661efaf623322fee9241d934
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-22 16:51:27 -0300

    logout de firebase

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,7 +1,18 @@
 import React from 'react'
+
+import { useDispatch } from 'react-redux';
+import { startLogout } from '../../actions/auth';
+
 import { JournalEntries } from './JournalEntries'
 
 export const Sidebar = () => {
+
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch( startLogout() );
+    }
+
     return (
         <aside className="journal__sidebar">
         
@@ -11,7 +22,10 @@ export const Sidebar = () => {
                     <span> Manuel</span>
                 </h3>
 
-                <button className="btn">
+                <button 
+                    className="btn"
+                    onClick={ handleLogout }
+                >
                     Logout
                 </button>
             </div>
