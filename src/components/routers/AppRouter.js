c028cea8d31a83c6dabe37262873c5f634fc231d
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-22 16:21:45 -0300

    cambios para mantener el estado de la autenticacion al refrescar pagina

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import { login } from '../../actions/auth';
+import { firebase } from '../../firebase/firebase-config';
 import { JournalScreen } from '../journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+    
+// Observable que va a estar pendientes en los cambios de autenticacion del usuario
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged( ( user ) => {
+            if ( user?.uid ) {
+                dispatch( login( user.uid, user.displayName ) );
+            }
+
+        });
+
+    }, [ dispatch ])
+
     return (
         <Router>
             <div>
