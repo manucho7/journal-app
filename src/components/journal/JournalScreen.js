9239ee091a23e9465cb872fb9fde401f7fc5d06d
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-23 11:25:17 -0300

    cambios

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NoteScreen } from '../notes/NoteScreen'
-// import { NothingSelected } from './NothingSelected'
 import { Sidebar } from './Sidebar'
+// import { NothingSelected } from './NothingSelected'
 
 export const JournalScreen = () => {
     return (
