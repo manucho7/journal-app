7f78b102803ed6c140446ff55f24171413c1e034
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-24 10:26:55 -0300

    Cargando las notas de firebase en state de app

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -9,6 +9,8 @@ import { JournalScreen } from '../journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
 import { PrivateRoute } from './PrivateRoute';
 import { PublicRoute } from './PublicRoute';
+import { loadNotes } from '../../helpers/loadNotes';
+import { setNotes } from '../../actions/notes';
 
 export const AppRouter = () => {
 
@@ -20,11 +22,18 @@ export const AppRouter = () => {
 
 // Observable que va a estar pendientes en los cambios de autenticacion del usuario
     useEffect(() => {
-        firebase.auth().onAuthStateChanged( ( user ) => {
+        firebase.auth().onAuthStateChanged( async ( user ) => {
             if ( user?.uid ) {
                 dispatch( login( user.uid, user.displayName ) );
+                
                 setIsLoggedIn(true);
+
+                const notes = await loadNotes( user.uid );
+
+                dispatch( setNotes( notes ) );
+                
             } else {
+
                 setIsLoggedIn(false);
             }
             setChecking(false);
