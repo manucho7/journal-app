aa35ca84ceba5ba3f1e0f86ad5e2d315aa7ca4db
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-24 09:17:22 -0300

    Generando nueva nota en firebase

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { useDispatch, useSelector } from 'react-redux';
 import { startLogout } from '../../actions/auth';
+import { startNewNote } from '../../actions/notes';
 
 import { JournalEntries } from './JournalEntries'
 
@@ -12,6 +13,10 @@ export const Sidebar = () => {
 
     const handleLogout = () => {
         dispatch( startLogout() );
+    }
+
+    const handleAddNew = () => {
+        dispatch( startNewNote() );
     }
 
     return (
@@ -31,7 +36,10 @@ export const Sidebar = () => {
                 </button>
             </div>
 
-            <div className="journal__new-entry">
+            <div 
+                className="journal__new-entry"
+                onClick={ handleAddNew }
+            >
                 <i className="far fa-calendar-plus fa-5x"></i>
                 <p className="mt-5">
                     New entry..
