f78055de6f081a49630d94b6c55e4d1bc4d250c7
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-28 10:13:22 -0300

    Borrando notas

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { activeNote } from '../../actions/notes';
+import { activeNote, startDeleting } from '../../actions/notes';
 import { useForm } from '../../hooks/useForm';
 import { NoteAppBar } from './NoteAppBar'
 
@@ -9,7 +9,7 @@ export const NoteScreen = () => {
     //renombrando active a note
     const { active:note } = useSelector(state => state.notes);
     const [ formValues, handleInputChange, reset ] = useForm( note );
-    const { body, title } = formValues;
+    const { body, title, id } = formValues;
 
 // ejecutar esa accion solo si el id de la nota es diferente,
 // caso contrario no lo tengo que disparar
@@ -27,6 +27,10 @@ export const NoteScreen = () => {
     useEffect(() => {
         dispatch( activeNote( formValues.id, { ...formValues } ) );
     }, [formValues, dispatch])
+
+    const handleDelete = () => {
+        dispatch( startDeleting(id) );
+    }
     
     return (
         <div className="notes__main-content">
@@ -63,6 +67,14 @@ export const NoteScreen = () => {
                     )
                 }
             </div>
+
+            <button 
+                className="btn btn-danger"
+                onClick={ handleDelete }
+            >
+                Delete
+            </button>
+
         </div>
     )
 }
