f2b82bec26f99a6df5049e4c9a973fbda365fe27
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-29 09:50:23 -0300

    borrando imagenes para no acumularlas en pruebas de cloudinary

--- src/tests/helpers/fileUpload.test.js
+++ src/tests/helpers/fileUpload.test.js
@@ -1,10 +1,17 @@
 import '@testing-library/dom';
 import '@testing-library/jest-dom';
 import { fileUpload } from '../../helpers/fileUpload';
+import cloudinary from 'cloudinary';
+
+cloudinary.config({ 
+    cloud_name: 'dzf9r3vek', 
+    api_key: '358411452477347', 
+    api_secret: 'KENErrk7X7LDMaLJw9masBZ8jOg' 
+});
 
 describe('Pruebas en fileUpload', () => {
     
-    test('debe de cargar un archivo y retornar el URL', async () => {
+    test('debe de cargar un archivo y retornar el URL', async ( done ) => {
         const resp = await fetch('https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Color_icon_green.svg/220px-Color_icon_green.svg.png')
         const blob = await resp.blob();
 
@@ -12,6 +19,12 @@ describe('Pruebas en fileUpload', () => {
         const url = await fileUpload( file );
 
         expect(typeof url ).toBe('string');
+
+        const segments = url.split('/');
+        const imageId = segments [ segments.length - 1 ].replace('.png', '')
+        cloudinary.v2.api.delete_resources(imageId, {}, () => {
+            done();
+        });
     });
     
     test('debe de retornar un error', async () => {
