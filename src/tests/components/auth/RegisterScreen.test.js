672d73e0880e567686f584f319418c55db886511
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-30 16:22:32 -0300

    Pruebas en <RegisterScreen />

---
+++ src/tests/components/auth/RegisterScreen.test.js
@@ -0,0 +1,98 @@
+import React from 'react';
+
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import { RegisterScreen } from '../../../components/auth/RegisterScreen';
+
+import '@testing-library/dom';
+import '@testing-library/jest-dom';
+import { types } from '../../../types/types';
+
+// jest.mock('../../../actions/auth', () => ({
+//     startGoogleLogin: jest.fn(),
+//     startLoginEmailPassword: jest.fn(),
+// }))
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const initState = {
+    auth: {},
+    ui: {
+        loading: false,
+        msgError: null
+    }
+};
+
+let store = mockStore( initState );
+// store.dispatch = jest.fn();
+
+const wrapper = mount(
+    <Provider store={ store }>
+        <MemoryRouter>
+            <RegisterScreen /> 
+        </MemoryRouter>
+    </Provider>
+);
+
+describe('Pruebas en <RegisterScreen />', () => {
+    
+    test('debe de mostrarse correctamente', () => {
+        
+        expect( wrapper ).toMatchSnapshot();
+
+    });
+    
+    test('debe de hacer el dispatch de la accion respectiva', () => {
+        
+        const emailField = wrapper.find('input[name="email"]');
+
+        emailField.simulate('change', {
+            target: {
+                value: '',
+                name: 'email'
+            }
+        });
+
+        wrapper.find('form').simulate('submit', {
+            preventDefault(){}
+        });
+
+        const actions = store.getActions();
+        expect( actions[0] ).toEqual({
+            type: types.uiSetError,
+            payload: 'invalid email'
+        });
+
+    });
+
+    test('debe de mostrar la caja de alerta con el error', () => {
+        
+        const initState = {
+            auth: {},
+            ui: {
+                loading: false,
+                msgError: 'invalid email'
+            }
+        };
+        
+        const store = mockStore( initState );
+        
+        const wrapper = mount(
+            <Provider store={ store }>
+                <MemoryRouter>
+                    <RegisterScreen /> 
+                </MemoryRouter>
+            </Provider>
+        );
+
+        expect( wrapper.find('.auth__alert-error').exists() ).toBe(true);
+        expect( wrapper.find('.auth__alert-error').text().trim() ).toBe( initState.ui.msgError );
+        
+    });
+    
+    
+})
