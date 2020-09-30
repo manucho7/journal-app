fc370a0e295b510dd0e9ec12bcb8f75c3e685afe
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-30 13:30:14 -0300

    pruebas en acciones startLogout y startLoginWithEmailPassword

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -9,7 +9,7 @@ export const startLoginEmailPassword = (email, password) => {
     return (dispatch) => {
         dispatch( startLoading() );
 
-        firebase.auth().signInWithEmailAndPassword( email, password )
+        return firebase.auth().signInWithEmailAndPassword( email, password )
             .then( ({ user }) => {
                 dispatch( login( user.uid, user.displayName ));
 
--- src/tests/actions/auth.test.js
+++ src/tests/actions/auth.test.js
@@ -1,9 +1,21 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { login, logout, startLoginEmailPassword, startLogout } from '../../actions/auth';
+import { types } from '../../types/types';
 import '@testing-library/dom';
 import '@testing-library/jest-dom';
-import { login, logout } from '../../actions/auth';
-import { types } from '../../types/types';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const initState = {};
+
+let store = mockStore( initState );
 
 describe('Pruebas con las acciones de auth', () => {
+    beforeEach( ()=> {
+        store = mockStore(initState);
+    })
     
     test('login y logout deben de crear la accion respectiva', () => {
         
@@ -29,9 +41,35 @@ describe('Pruebas con las acciones de auth', () => {
 
     });
 
-    test('debe de realizar el logout', () => {
+    test('debe de realizar el logout y notesLogoutCleaning', async () => {
         
+        await store.dispatch( startLogout() );
+
+        const actions = store.getActions();
+
+        expect( actions[0] ).toEqual({
+            type: types.logout
+        });
+
+        expect( actions[1] ).toEqual({
+            type: types.notesLogoutCleaning
+        });
+
+    });
+
+    test('debe de iniciar el startLoginWithEmailPassword', async () => {
+        await store.dispatch( startLoginEmailPassword( 'test@testing.com', '123456') );
+        
+        const actions = store.getActions();
+        
+        expect( actions[1] ).toEqual({
+            type: types.login,
+            payload: {
+                uid: 'GHH8Uj5zTGVGxh8DFVlyr4pRI2C3',
+                displayName: null
+            }
+        });
+    
     });
     
-
 })
